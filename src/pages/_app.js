2c7e63d4074fb866660fc1a889c6bdb1087cba88
DeShon Dixon <deshondixon@gmail.com>
2023-03-28 14:08:20 -0700

    acp

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,37 @@
 import '@/styles/globals.css';
-import { NextUIProvider } from '@nextui-org/react';
+import { createTheme, NextUIProvider } from '@nextui-org/react';
+import { ThemeProvider as NextThemesProvider } from 'next-themes';
 
-export default function App({ Component, pageProps }) {
+// 2. Call `createTheme` and pass your custom values
+const lightTheme = createTheme({
+  type: 'light',
+  theme: {
+    colors: {}, // optional
+  },
+});
+
+const darkTheme = createTheme({
+  type: 'dark',
+  theme: {
+    colors: {}, // optional
+  },
+});
+
+function MyApp({ Component, pageProps }) {
   return (
-    <NextUIProvider>
-      <Component {...pageProps} />
-    </NextUIProvider>
+    <NextThemesProvider
+      defaultTheme='system'
+      attribute='class'
+      value={{
+        light: lightTheme.className,
+        dark: darkTheme.className,
+      }}
+    >
+      <NextUIProvider>
+        <Component {...pageProps} />
+      </NextUIProvider>
+    </NextThemesProvider>
   );
 }
+
+export default MyApp;
