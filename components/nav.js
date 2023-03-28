be900841417fdd2a87c5ce2dbad45a375597b556
DeShon Dixon <deshondixon@gmail.com>
2023-03-28 13:27:13 -0700

    acp

--- components/nav.js
+++ components/nav.js
@@ -1,13 +1,15 @@
+import React from 'react';
+
 export default function Nav() {
   return (
     <>
       <h1 className=''>DeShon Dixon</h1>
-      <div>
+      <React.Fragment>
         <div>HOME</div>
         <div>PROJECTS</div>
         <div>ABOUT</div>
         <div>CONTACT</div>
-      </div>
+      </React.Fragment>
     </>
   );
 }
