5c41dd71524047ab2aecedc84c362ef84eac42aa
DeShon Dixon <deshondixon@gmail.com>
2023-03-28 14:17:34 -0700

    acp

--- components/nav.js
+++ components/nav.js
@@ -1,14 +1,24 @@
 import React from 'react';
+import { Text } from '@nextui-org/react';
 
 export default function Nav() {
   return (
     <>
       <div className='flex flex-row items-center justify-start p-8 text-xl'>
         <div>
-          <h1 className='text-5xl font-bold text-'>DD</h1>
+          <Text
+            h1
+            size={60}
+            css={{
+              textGradient: '45deg, $yellow600 -20%, $red600 100%',
+            }}
+            weight='bold'
+          >
+            DD
+          </Text>
         </div>
-        <div className='flex pl-8 justify-evenly'>
-          <div className='flex justify-evenly'>HOME</div>
+        <div className='flex justify-end pl-8'>
+          <div className='flex justify-end'>HOME</div>
           <div>PROJECTS</div>
           <div>ABOUT</div>
           <div>CONTACT</div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import Nav from '/components/nav';
+import { Text } from '@nextui-org/react';
 
 export default function Home() {
   return (
     <>
       <Nav />
-      <h1 className='text-3xl font-bold underline'>DeShon Dixon</h1>
-      <p>I am a Software Developer based in Washington.</p>
+      <div className='flex flex-col justify-center'>
+        <Text
+          className='flex justify-center text-6xl font-bold underline'
+          h1
+          size={60}
+          css={{
+            textGradient: '45deg, $blue600 -20%, $pink600 50%',
+          }}
+          weight='bold'
+        >
+          DESHON DIXON
+        </Text>
+        <p className='flex justify-center text-xl'>
+          I am a Software Developer based in Washington.
+        </p>
+      </div>
     </>
   );
 }
