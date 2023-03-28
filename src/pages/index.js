b74d9646e22bb57a1a3f6e37271318002e4e25ed
DeShon Dixon <deshondixon@gmail.com>
2023-03-28 13:29:43 -0700

    acp

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import Nav from '/components/nav';
 
 export default function Home() {
   return (
     <>
+      <Nav />
       <h1 className='text-3xl font-bold underline'>DeShon Dixon</h1>
       <p>I am a Software Developer based in Washington.</p>
     </>
