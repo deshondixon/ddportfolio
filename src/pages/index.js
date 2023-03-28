e969a9b11e61774a8b4a7b339b9072e15e5e6c25
DeShon Dixon <deshondixon@gmail.com>
2023-03-28 13:22:46 -0700

    acp

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,8 @@
 export default function Home() {
-  return <h1 className='text-3xl font-bold underline'>DeShon Dixon</h1>;
+  return (
+    <>
+      <h1 className='text-3xl font-bold underline'>DeShon Dixon</h1>
+      <p>I am a Software Developer based in Washington.</p>
+    </>
+  );
 }
