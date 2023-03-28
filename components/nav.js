7b5a5551bf44afbc116ca423b5cef9a3eed7dd9d
DeShon Dixon <deshondixon@gmail.com>
2023-03-28 13:28:14 -0700

    acp

--- components/nav.js
+++ components/nav.js
@@ -5,10 +5,10 @@ export default function Nav() {
     <>
       <h1 className=''>DeShon Dixon</h1>
       <React.Fragment>
-        <div>HOME</div>
-        <div>PROJECTS</div>
-        <div>ABOUT</div>
-        <div>CONTACT</div>
+        <ul>HOME</ul>
+        <ul>PROJECTS</ul>
+        <ul>ABOUT</ul>
+        <ul>CONTACT</ul>
       </React.Fragment>
     </>
   );
