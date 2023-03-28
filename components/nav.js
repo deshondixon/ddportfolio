3ea630e56b25b571500d0b629b2e2e3c6077973f
DeShon Dixon <deshondixon@gmail.com>
2023-03-28 13:40:23 -0700

    acp

--- components/nav.js
+++ components/nav.js
@@ -3,13 +3,15 @@ import React from 'react';
 export default function Nav() {
   return (
     <>
-      <div className='flex flex-row text-xl justify-evenly'>
-        <h1>DeShon Dixon</h1>
-        <div className='flex justify-evenly'>
-          <ul>HOME</ul>
-          <ul>PROJECTS</ul>
-          <ul>ABOUT</ul>
-          <ul>CONTACT</ul>
+      <div className='flex flex-row items-center justify-start p-3 text-xl'>
+        <div>
+          <h1 className=''>DD</h1>
+        </div>
+        <div className='flex pl-8 justify-evenly'>
+          <div className='flex justify-evenly'>HOME</div>
+          <div>PROJECTS</div>
+          <div>ABOUT</div>
+          <div>CONTACT</div>
         </div>
       </div>
     </>
