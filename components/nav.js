4940571bb0228d54dfcbee2291511d1d4df14706
DeShon Dixon <deshondixon@gmail.com>
2023-03-28 13:26:04 -0700

    acp

---
+++ components/nav.js
@@ -0,0 +1,13 @@
+export default function Nav() {
+  return (
+    <>
+      <h1 className=''>DeShon Dixon</h1>
+      <div>
+        <div>HOME</div>
+        <div>PROJECTS</div>
+        <div>ABOUT</div>
+        <div>CONTACT</div>
+      </div>
+    </>
+  );
+}
