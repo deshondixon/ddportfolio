4d0d536e893131b625101f1c82f58c1762d37e0d
DeShon Dixon <deshondixon@gmail.com>
2023-03-28 13:43:10 -0700

    acp

--- components/nav.js
+++ components/nav.js
@@ -3,9 +3,9 @@ import React from 'react';
 export default function Nav() {
   return (
     <>
-      <div className='flex flex-row items-center justify-start p-3 text-xl'>
+      <div className='flex flex-row items-center justify-start p-8 text-xl'>
         <div>
-          <h1 className=''>DD</h1>
+          <h1 className='text-5xl font-bold text-'>DD</h1>
         </div>
         <div className='flex pl-8 justify-evenly'>
           <div className='flex justify-evenly'>HOME</div>
