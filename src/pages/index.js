91a51e69adc437978f97cff19f89b3bbf2fa72fe
DeShon Dixon <deshondixon@gmail.com>
2023-03-30 22:16:11 -0700

    acp

--- src/pages/index.js
+++ src/pages/index.js
@@ -23,6 +23,18 @@ export default function Home() {
         <p className='flex justify-center text-xl'>
           I am a Software Developer based in Washington.
         </p>
+         <div className='flex flex-col justify-center'>
+        <Text
+          className='flex justify-center text-6xl font-bold underline'
+          h1
+          size={60}
+          css={{
+            textGradient: '45deg, $blue600 -20%, $pink600 50%',
+          }}
+          weight='bold'
+        >
+          BIO
+        </Text>
       </div>
     </>
   );
