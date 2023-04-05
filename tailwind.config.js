819fe3ece0eff6d983353b6e65621ab5bfa0d69c
DeShon Dixon <deshondixon@gmail.com>
2023-04-04 20:25:45 -0700

    acp

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  purge: ['./pages/**/*.tsx', './components/**/*.tsx'],
-  darkMode: false,
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
