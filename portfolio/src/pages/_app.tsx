ecf4ecfa47b8c6e9751e7864d7ef9e600f812a22
DeShon Dixon <deshondixon@gmail.com>
2023-04-04 21:02:27 -0700

    acp

--- portfolio/src/pages/_app.tsx
+++ portfolio/src/pages/_app.tsx
@@ -1,6 +1,15 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <div className='grid grid-col-12 gap-6 my-14 px-48'>
+        <div className='grid grid-col-3 bg-white rounded-2xl'>Sidebar</div>
+        <div className='grid grid-col-9 bg-white'>
+          <Component {...pageProps} />
+        </div>
+      </div>
+    </>
+  );
 }
