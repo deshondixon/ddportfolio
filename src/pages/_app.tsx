bf7031d040e10a790a53a25667f090bd29718169
DeShon Dixon <deshondixon@gmail.com>
2023-04-04 20:24:03 -0700

    acp

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,38 +1,13 @@
-import { createTheme, NextUIProvider } from '@nextui-org/react';
-import { ThemeProvider as NextThemesProvider } from 'next-themes';
-
-// 2. Call `createTheme` and pass your custom values
-const lightTheme = createTheme({
-  type: 'light',
-  theme: {
-    colors: {}, // optional
-  },
-});
-
-const darkTheme = createTheme({
-  type: 'dark',
-  theme: {
-    colors: {}, // optional
-  },
-});
+import { NextUIProvider } from '@nextui-org/react';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <NextThemesProvider
-      defaultTheme='system'
-      attribute='class'
-      value={{
-        light: lightTheme.className,
-        dark: darkTheme.className,
-      }}
-    >
-      <NextUIProvider>
-        <div className='grid gap-6 grid-col-12'>
-          <div>Sidebar</div>
-        </div>
+    <div className='grid gap-6 grid-col-12'>
+      <div className='col-span-3 text-3xl text-red-500'>Sidebar</div>
+      <div className='col-span-9 bg-white'>
         <Component {...pageProps} />
-      </NextUIProvider>
-    </NextThemesProvider>
+      </div>
+    </div>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './app/**/*.{js,ts,jsx,tsx}',
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-
-    // Or if using `src` directory:
-    './src/**/*.{js,ts,jsx,tsx}',
-  ],
-  darkMode: true,
+  purge: ['./pages/**/*.tsx', './components/**/*.tsx'],
+  darkMode: false,
   theme: {
     extend: {},
   },
