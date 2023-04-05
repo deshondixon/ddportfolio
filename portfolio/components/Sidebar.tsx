964d9c334a893b87d65017c0a959c6ae02105d5e
DeShon Dixon <deshondixon@gmail.com>
2023-04-04 21:09:38 -0700

    acp

---
+++ portfolio/components/Sidebar.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Sidebar = () => {
+  return (
+    <div className='lg:col-span-3 bg-white rounded-2xl col-span-12 p-4 text-center'>
+      Sidebar
+    </div>
+  );
+};
+
+export default Sidebar;
--- portfolio/src/pages/_app.tsx
+++ portfolio/src/pages/_app.tsx
@@ -1,12 +1,13 @@
 import '@/styles/globals.css';
+import { Sidebar } from 'components/sidebar';
 import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <div className='grid grid-cols-12 gap-6 my-14 px-48'>
-        <div className='col-span-3 bg-white rounded-2xl'>Sidebar</div>
-        <div className='col-span-9 bg-white rounded-2xl'>
+      <div className='grid grid-cols-12 gap-6 my-14 lg:px-48 px-5'>
+        <Sidebar />
+        <div className='lg:col-span-9 bg-white rounded-2xl col-span-12'>
           <Component {...pageProps} />
         </div>
       </div>
