1d931432bb0f7648c94b2a93e05be1006bebb6bf
DeShon Dixon <deshondixon@gmail.com>
2023-04-04 21:23:38 -0700

    acp

--- portfolio/components/Sidebar.tsx
+++ portfolio/components/Sidebar.tsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import Image from 'next/image';
 
 const Sidebar = () => {
   return (
-    <div className='lg:col-span-3 bg-white rounded-2xl col-span-12 p-4 text-center'>
-      Sidebar
+    <div className=''>
+      <Image
+        src='https://media.licdn.com/dms/image/D5603AQG8ud8c-q7WHg/profile-displayphoto-shrink_800_800/0/1672429025757?e=2147483647&v=beta&t=MATrpoJPnAcYw5NFzME5TINvD6-iTWL431yoAy88clQ'
+        alt='avatar'
+      />
+      <h3>
+        <span>DeShon</span>
+        Dixon
+      </h3>
+      <p>Software Engineer</p>
+      <p>Download Resume</p>
     </div>
   );
 };
--- portfolio/src/pages/_app.tsx
+++ portfolio/src/pages/_app.tsx
@@ -1,16 +1,18 @@
 import '@/styles/globals.css';
-import { Sidebar } from 'components/sidebar';
+import Sidebar from 'portfolio/Sidebar.js';
 import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <div>
       <div className='grid grid-cols-12 gap-6 my-14 lg:px-48 px-5'>
-        <Sidebar />
+        <div className='lg:col-span-3 bg-white rounded-2xl col-span-12 p-4 text-center'>
+          <Sidebar />
+        </div>
         <div className='lg:col-span-9 bg-white rounded-2xl col-span-12'>
           <Component {...pageProps} />
         </div>
       </div>
-    </>
+    </div>
   );
 }
