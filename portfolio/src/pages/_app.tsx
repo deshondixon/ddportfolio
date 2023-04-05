b7dab41ba5e6bbe31bab3bad8c63fa7b21814f3c
DeShon Dixon <deshondixon@gmail.com>
2023-04-04 21:03:47 -0700

    acp

--- portfolio/src/pages/_app.tsx
+++ portfolio/src/pages/_app.tsx
@@ -4,9 +4,9 @@ import type { AppProps } from 'next/app';
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <div className='grid grid-col-12 gap-6 my-14 px-48'>
-        <div className='grid grid-col-3 bg-white rounded-2xl'>Sidebar</div>
-        <div className='grid grid-col-9 bg-white'>
+      <div className='grid grid-cols-12 gap-6 my-14 px-48'>
+        <div className='col-span-3 bg-white rounded-2xl'>Sidebar</div>
+        <div className='col-span-9 bg-white rounded-2xl'>
           <Component {...pageProps} />
         </div>
       </div>
