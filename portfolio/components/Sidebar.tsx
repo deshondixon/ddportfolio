9bda03122050b56f976d23fb7a5e2df38c92d191
DeShon Dixon <deshondixon@gmail.com>
2023-04-05 20:38:55 -0700

    acp

--- portfolio/components/Sidebar.tsx
+++ portfolio/components/Sidebar.tsx
@@ -3,10 +3,6 @@ import Image from 'next/image';
 const Sidebar = () => {
   return (
     <div className=''>
-      <Image
-        src='https://media.licdn.com/dms/image/D5603AQG8ud8c-q7WHg/profile-displayphoto-shrink_800_800/0/1672429025757?e=2147483647&v=beta&t=MATrpoJPnAcYw5NFzME5TINvD6-iTWL431yoAy88clQ'
-        alt='avatar'
-      />
       <h3>
         <span>DeShon</span>
         Dixon
