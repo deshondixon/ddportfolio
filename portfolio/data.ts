738c00df680340ceb32dacc2f4e15715bf6e49d2
DeShon Dixon <deshondixon@gmail.com>
2023-04-11 16:32:41 -0700

    acp

--- portfolio/data.ts
+++ portfolio/data.ts
@@ -3,13 +3,24 @@ import { IService } from './type';
 
 export const services: IService[] = [
   {
-    title: 'Frontend Dev',
-    about: 'lorem....',
     Icon: RiComputerLine,
+    title: 'Frontend Development',
+    about:
+      'HTML, CSS, Javascript, Bootstrap, React.js, React Native, Next.js, Tailwind, Chakra UI, Astro',
   },
   {
+    Icon: RiComputerLine,
     title: 'title',
     about: 'lorem....',
+  },
+  {
     Icon: RiComputerLine,
+    title: 'title',
+    about: 'lorem....',
+  },
+  {
+    Icon: RiComputerLine,
+    title: 'title',
+    about: 'lorem....',
   },
 ];
