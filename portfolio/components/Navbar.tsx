170eadc747f63341558c89e818d2be438fd5df4a
DeShon Dixon <deshondixon@gmail.com>
2023-04-10 18:40:51 -0700

    acp

--- portfolio/components/Navbar.tsx
+++ portfolio/components/Navbar.tsx
@@ -1,3 +1,35 @@
+import { useState } from 'react';
+import Link from 'next/link';
+
 export default function Navbar({}) {
-  return <div className=''>Navbar</div>;
+  const [activeItem, setActiveItem] = useState<string>('');
+
+  return (
+    <div>
+      <span>{activeItem}</span>
+      <div>
+        {activeItem !== 'About' && (
+          <Link href='/'>
+            <a>
+              <span>About</span>
+            </a>
+          </Link>
+        )}
+        {activeItem !== 'Projects' && (
+          <Link href='/'>
+            <a>
+              <span>Projects</span>
+            </a>
+          </Link>
+        )}
+        {activeItem !== 'Resume' && (
+          <Link href='/'>
+            <a>
+              <span>Resume</span>
+            </a>
+          </Link>
+        )}
+      </div>
+    </div>
+  );
 }
