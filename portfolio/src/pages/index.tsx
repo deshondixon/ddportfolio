4dbced91d3ee656b2754dedeb6e3f566e40372f5
DeShon Dixon <deshondixon@gmail.com>
2023-04-11 17:28:50 -0700

    acp

--- portfolio/src/pages/index.tsx
+++ portfolio/src/pages/index.tsx
@@ -1,6 +1,7 @@
 import { services } from '/Users/deshondixon/projects/side_projects/ddportfolio/portfolio/data.ts';
+import { GetServerSidePropsContext } from 'next';
 
-const index = () => {
+const index = (props) => {
   console.log(services);
 
   return (
@@ -11,3 +12,11 @@ const index = () => {
 };
 
 export default index;
+
+export const getServerSideProps = (context: GetServerSidePropsContext) => {
+  fetch('http://localhost:3000/api/services');
+
+  return {
+    props: {},
+  };
+};
