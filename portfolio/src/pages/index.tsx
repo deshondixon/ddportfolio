1bf12bc9b90d82e25a3daf83240cbe5252b41f54
DeShon Dixon <deshondixon@gmail.com>
2023-04-11 17:55:51 -0700

    acp

--- portfolio/src/pages/index.tsx
+++ portfolio/src/pages/index.tsx
@@ -11,22 +11,9 @@ const index = ({ services }) => {
 
 export default index;
 
-// export const getServerSideProps = async (
-//   context: GetServerSidePropsContext
-// ) => {
-//   const res = await fetch('http://localhost:3000/api/services');
-//   const data = await res.json();
-
-//   console.log('SERVER', services);
-
-//   return {
-//     props: {
-//       services: data.services,
-//     },
-//   };
-// };
-
-export const getStaticProps = async (context: GetStaticPropsContext) => {
+export const getServerSideProps = async (
+  context: GetServerSidePropsContext
+) => {
   const res = await fetch('http://localhost:3000/api/services');
   const data = await res.json();
 
@@ -38,3 +25,17 @@ export const getStaticProps = async (context: GetStaticPropsContext) => {
     },
   };
 };
+
+// export const getStaticProps = async (context: GetStaticPropsContext) => {
+//   const res = await fetch('http://localhost:3000/api/services');
+//   const data = await res.json();
+
+//   console.log('SERVER', services);
+
+//   return {
+//     props: {
+//       services: data.services,
+//       fallback: false,
+//     },
+//   };
+// };
