45d3f0b2aa854dcddef3eb201203c5d8b6f56cf0
DeShon Dixon <deshondixon@gmail.com>
2023-04-11 17:06:48 -0700

    acp

--- portfolio/src/pages/index.tsx
+++ portfolio/src/pages/index.tsx
@@ -1,4 +1,8 @@
+import { services } from '/Users/deshondixon/projects/side_projects/ddportfolio/portfolio/data.ts';
+
 const index = () => {
+  console.log(services);
+
   return (
     <div>
       <h1 className=''>Hello world!</h1>
