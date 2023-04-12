ff2293e9a8d5219454f46f011231c92d2552ee50
DeShon Dixon <deshondixon@gmail.com>
2023-04-11 18:10:37 -0700

    acp

--- portfolio/src/pages/index.tsx
+++ portfolio/src/pages/index.tsx
@@ -1,30 +1,40 @@
 import { services } from '/Users/deshondixon/projects/side_projects/ddportfolio/portfolio/data';
 import { GetServerSidePropsContext, GetStaticPropsContext } from 'next';
+import { Text } from '@nextui-org/react';
 
 const index = ({ services }) => {
+  console.log(services);
   return (
-    <div>
-      <h1 className=''>Hello world!</h1>
+    <div className='flex flex-col flex-grow px-6 pt-1 '>
+      <Text className='my-3 text-base font-medium'>
+        Software Engineer and Army Veteran offering a strong foundation in
+        software development and programming principles across multiple
+        platforms. Offers knowledge of authoring code derived from designing,
+        building, and maintaining websites using scripting languages, content
+        creation tools, and digital media. Demonstrated history of producing
+        high-quality project results; possesses an innate talent for quickly
+        mastering technology and new concepts.
+      </Text>
     </div>
   );
 };
 
 export default index;
 
-export const getServerSideProps = async (
-  context: GetServerSidePropsContext
-) => {
-  const res = await fetch('http://localhost:3000/api/services');
-  const data = await res.json();
+// export const getServerSideProps = async (
+//   context: GetServerSidePropsContext
+// ) => {
+//   const res = await fetch('http://localhost:3000/api/services');
+//   const data = await res.json();
 
-  console.log('SERVER', services);
+//   console.log('SERVER', services);
 
-  return {
-    props: {
-      services: data.services,
-    },
-  };
-};
+//   return {
+//     props: {
+//       services: data.services,
+//     },
+//   };
+// };
 
 // export const getStaticProps = async (context: GetStaticPropsContext) => {
 //   const res = await fetch('http://localhost:3000/api/services');
@@ -35,7 +45,6 @@ export const getServerSideProps = async (
 //   return {
 //     props: {
 //       services: data.services,
-//       fallback: false,
 //     },
 //   };
 // };
