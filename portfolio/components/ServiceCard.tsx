8d0c0ec3c7b04343eb215afcf04928db7513ac65
DeShon Dixon <deshondixon@gmail.com>
2023-04-11 18:20:43 -0700

    acp

--- portfolio/components/ServiceCard.tsx
+++ portfolio/components/ServiceCard.tsx
@@ -1,8 +1,19 @@
 import React, { FunctionComponent } from 'react';
 import { IService } from '../type';
+import { Text } from '@nextui-org/react';
 
-const ServiceCard: FunctionComponent<{ service: IService }> = ({ service }) => {
-  return <div>ServiceCard</div>;
+const ServiceCard: FunctionComponent<{ service: IService }> = ({
+  service: { Icon, about, title },
+}) => {
+  return (
+    <div>
+      <Icon />
+      <div>
+        <Text>{title}</Text>
+        <Text>{about}</Text>
+      </div>
+    </div>
+  );
 };
 
 export default ServiceCard;
