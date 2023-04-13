d8236bf0ff961ee4ce77e696a83a47fc1305d83c
DeShon Dixon <deshondixon@gmail.com>
2023-04-12 21:22:26 -0700

    acp

--- portfolio/src/pages/resume.tsx
+++ portfolio/src/pages/resume.tsx
@@ -5,7 +5,14 @@ const resume = () => {
     <div className='p-4'>
       <div className='grid gap-6 md:grid-cols-2'>
         <div>
-          <Text className='my-3 text-lg tracking-normal' weight='normal'>
+          <Text
+            size={35}
+            css={{
+              textGradient: '45deg, $yellow600 -20%, $red600 100%',
+            }}
+            weight='bold'
+            className='my-4 text-3xl font-medium tracking-wider'
+          >
             Education
           </Text>
           <div></div>
