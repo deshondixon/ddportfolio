01a7760eb4feef602826f9a21cefba99e8872663
DeShon Dixon <deshondixon@gmail.com>
2023-04-12 21:52:25 -0700

    acp

---
+++ portfolio/components/Bar.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { FunctionComponent } from 'react';
+import { ISkill } from '/Users/deshondixon/projects/side_projects/ddportfolio/portfolio/type';
+
+const Bar: FunctionComponent<{
+  data: ISkill;
+}> = () => {
+  return <div>Bar</div>;
+};
+
+export default Bar;
