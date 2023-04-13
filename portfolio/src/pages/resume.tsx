9b18254e75ade6d2c4a6c619f2c6808f314ba50c
DeShon Dixon <deshondixon@gmail.com>
2023-04-12 21:21:16 -0700

    acp

--- portfolio/src/pages/resume.tsx
+++ portfolio/src/pages/resume.tsx
@@ -1,5 +1,18 @@
+import { Text } from '@nextui-org/react';
+
 const resume = () => {
-  return <div className='p-4'>resume</div>;
+  return (
+    <div className='p-4'>
+      <div className='grid gap-6 md:grid-cols-2'>
+        <div>
+          <Text className='my-3 text-lg tracking-normal' weight='normal'>
+            Education
+          </Text>
+          <div></div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default resume;
