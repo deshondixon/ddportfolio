aff84c029bec75f884001308141fdac278490f09
DeShon Dixon <deshondixon@gmail.com>
2023-04-25 21:58:27 -0700

    acp

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,7 @@ import Skeleton from 'react-loading-skeleton';
 import { createTheme, NextUIProvider } from '@nextui-org/react';
 import { ThemeProvider as NextThemesProvider, useTheme } from 'next-themes';
 import { useState, useEffect } from 'react';
+import { Analytics } from '@vercel/analytics';
 
 const lightTheme = createTheme({
   type: 'light',
@@ -48,6 +49,7 @@ export default function MyApp({ Component, pageProps }) {
               ) : (
                 <Component {...pageProps} />
               )}
+              <Analytics />
             </div>
           </div>
         </div>
