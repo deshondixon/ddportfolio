e629496cb7c738a1c0efeab6eb4a66cd169f78b8
DeShon Dixon <deshondixon@gmail.com>
2023-04-27 16:36:41 -0700

    acp

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,50 @@
+import '../styles/globals.css';
+import Sidebar from '../../components/Sidebar';
+import Navbar from '../../components/Navbar';
+import Skeleton from 'react-loading-skeleton';
+import { createTheme, NextUIProvider } from '@nextui-org/react';
+import { useState, useEffect } from 'react';
+import { Analytics } from '@vercel/analytics/react';
+import { AppProps } from 'next/app';
+import { ThemeProvider as NextThemesProvider } from 'next-themes';
+import 'tailwindcss/tailwind.css';
 
+const darkTheme = createTheme({
+  type: 'dark',
+  theme: {},
+});
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 0);
+  }, []);
+
+  return (
+    <div>
+      <NextThemesProvider
+        defaultTheme='system'
+        attribute='class'
+        value={{
+          dark: darkTheme.className,
+        }}
+      >
+        <NextUIProvider>
+          <div className='grid grid-cols-12 gap-6 px-5 pt-14 lg:px-48'>
+            <div className='col-span-12 p-4 text-center lg:col-span-3 rounded-2xl glass'>
+              {loading ? <Skeleton height={500} /> : <Sidebar />}
+            </div>
+            <div className='col-span-12 overflow-hidden glass lg:col-span-9 rounded-2xl'>
+              <Navbar />
+
+              {loading ? (
+                <Skeleton height={2000} />
+              ) : (
+                <Component {...pageProps} />
+              )}
+              <Analytics />
             </div>
           </div>
         </NextUIProvider>
