499abadffc52670dd83bdc64e98ae21dc9a0c4a3
DeShon Dixon <deshondixon@gmail.com>
2023-05-01 05:43:52 -0700

    acp

--- components/ServiceCard.tsx
+++ components/ServiceCard.tsx
@@ -1,4 +1,14 @@
-
+flex items-center p-2space-x-7'>
+      <Icon className='w-16 h-16' />
+      <div className='items-center'>
+        <Text
+          size={18}
+          css={{
+            textGradient: '45deg, $purple600 -20%, $pink600 100%',
+          }}
+          weight='bold'
+          className='text-3xl font-medium tracking-wider text-white'
+        >
           {title}
         </Text>
         <Text
