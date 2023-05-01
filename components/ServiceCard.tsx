5b7a22adb2110d1f4f0b24fd528ffd0e9e482f0d
DeShon Dixon <deshondixon@gmail.com>
2023-05-01 05:43:57 -0700

    acp

--- components/ServiceCard.tsx
+++ components/ServiceCard.tsx
@@ -1,4 +1,17 @@
-flex items-center p-2space-x-7'>
+import React, { FunctionComponent } from 'react';
+import { IService } from '../src/type';
+import { Text, Textarea } from '@nextui-org/react';
+
+const ServiceCard: FunctionComponent<{ service: IService }> = ({
+  service: { Icon, about, title },
+}) => {
+  const createMarkup = () => {
+    return {
+      __html: about,
+    };
+  };
+  return (
+    <div className='flex items-center p-2space-x-7'>
       <Icon className='w-16 h-16' />
       <div className='items-center'>
         <Text
