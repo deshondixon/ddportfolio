ddfb93bd77aa637cb137f3ee24b27bfdc4c350c0
DeShon Dixon <deshondixon@gmail.com>
2023-05-02 15:42:48 -0700

    acp

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -3,7 +3,17 @@ import { IProject } from '../src/type';
 
 const ProjectCard: FunctionComponent<{
   project: IProject;
-}> = () => {
+}> = ({
+  project: {
+    name,
+    image_path,
+    category,
+    deployed_url,
+    description,
+    github_url,
+    key_techs,
+  },
+}) => {
   return <div>ProjectCard</div>;
 };
 
