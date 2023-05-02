18c54847939493b285da1b8eab307a27ab3ff9b4
DeShon Dixon <deshondixon@gmail.com>
2023-05-02 14:30:32 -0700

    acp

--- src/type.ts
+++ src/type.ts
@@ -11,3 +11,12 @@ export interface ISkill {
   level: string;
   Icon: IconType;
 }
+
+export interface IProject {
+  name: string;
+  description: string;
+  image_path: string;
+  deployed_url: string;
+  github_url: string;
+  category: string;
+}
