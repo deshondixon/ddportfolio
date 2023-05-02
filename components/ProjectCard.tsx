1082405fc379f1d310af35fad431fcb5b4c70db3
DeShon Dixon <deshondixon@gmail.com>
2023-05-02 16:00:05 -0700

    acp

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -1,5 +1,7 @@
 import { FunctionComponent } from 'react';
 import { IProject } from '../src/type';
+import { Text } from '@nextui-org/react';
+import Image from 'next/image';
 
 const ProjectCard: FunctionComponent<{
   project: IProject;
@@ -14,7 +16,27 @@ const ProjectCard: FunctionComponent<{
     key_techs,
   },
 }) => {
-  return <div>{name}</div>;
+  return (
+    <div>
+      <Image
+        src={image_path}
+        alt={name}
+        className='cursor-pointer'
+        width={500}
+        height={500}
+      />
+      <Text
+        size={20}
+        css={{
+          textGradient: '45deg, $yellow600 -20%, $red600 100%',
+        }}
+        weight='bold'
+        className='my-2 text-3xl font-medium tracking-wider text-center'
+      >
+        {name}
+      </Text>
+    </div>
+  );
 };
 
 export default ProjectCard;
