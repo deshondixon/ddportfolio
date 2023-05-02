b43a3262900798633e86129ef154ace18afa24c0
DeShon Dixon <deshondixon@gmail.com>
2023-05-02 15:51:26 -0700

    acp

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,16 +1,20 @@
+import ProjectCard from '../../components/ProjectCard';
 import { projects } from '../data';
 
 const Projects = () => {
   return (
-    <div>
-      <div className='px-6 py-2'>
-        <nav>Navbar</nav>
+    <div className='px-6 py-2'>
+      <nav>Navbar</nav>
+      <div className='grid grid-cols-12 gap-4 my-3'>
+        {projects.map((project) => (
+          <div
+            className='col-span-12 p-2 sm:col-span-6 lg:col-span-4'
+            key={project.name}
+          >
+            <ProjectCard project={project} />
+          </div>
+        ))}
       </div>
-      {projects.map((projects) => (
-        <div>
-          <ProjectCard />
-        </div>
-      ))}
     </div>
   );
 };
