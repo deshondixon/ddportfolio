21d33699fbc33d60ba79aafaaeeb974c26ecfada
DeShon Dixon <deshondixon@gmail.com>
2023-05-02 15:11:32 -0700

    acp

--- src/data.ts
+++ src/data.ts
@@ -103,10 +103,20 @@ export const projects: IProject[] = [
     name: 'OpenImg',
     description:
       'OpenImg is an AI-powered image rendering platform that allows users to turn their creative ideas into realistic images. With OpenImg, users can simply input their desired image concept and the AI technology will generate a high-quality rendering of the image. The platform also features a login system where users can save their rendered images to their personal collection for future use or reference.',
-    image_path: '/images/jobhunter5.png',
-    deployed_url: 'https://github.com/CodingAcademics/JobHunter',
+    image_path: '/images/openimg1.png',
+    deployed_url: 'https://openimg.netlify.app/',
     github_url: 'https://github.com/orgs/Full-Stack-Fluency/repositories',
     category: ['react'],
     key_techs: ['react', 'mongodb', 'express', 'node'],
+  },
+  {
+    name: 'Pigs and Bulls',
+    description:
+      'As a developer, I created a word-guessing game using React, JavaScript, and CSS. The game is similar to Wordle, in which the player is required to guess a 5-letter word. Upon guessing the word, the game will inform the player how many letters are correct (pigs) and how many words are correct and in the correct spot (bulls). I utilized React for the front-end of the game, allowing for a smooth and interactive user experience. The game logic was implemented using JavaScript, and the overall design and styling was achieved through the use of CSS. Overall, it was a great opportunity for me to further enhance my skills in these technologies and create a fun and engaging game for players.',
+    image_path: '/images/openimg1.png',
+    deployed_url: 'https://thewordgame.github.io/pigs-and-bulls/',
+    github_url: 'https://github.com/thewordgame/pigs-and-bulls',
+    category: ['react'],
+    key_techs: ['react'],
   },
 ];
