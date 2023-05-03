7b03efca97c67d0e15565338f884dc36753f0918
DeShon Dixon <deshondixon@gmail.com>
2023-05-02 22:02:43 -0700

    acp

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -17,7 +17,7 @@ const NavItem: FunctionComponent<{
           textGradient: '45deg, $yellow600 -20%, $red600 100%',
         }}
         weight='bold'
-        className='text-3xl font-medium tracking-wider'
+        className='text-3xl font-medium tracking-wider '
       >
         <span onClick={() => setActiveItem(name)}>{name}</span>
       </Text>
@@ -49,7 +49,7 @@ const Navbar = () => {
         {activeItem}
       </Text>
 
-      <div className='flex space-x-3'>
+      <div className='flex items-center justify-center flex-shrink pl-4 space-x-2'>
         <NavItem
           activeItem={activeItem}
           setActiveItem={setActiveItem}
