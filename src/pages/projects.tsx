de8bcf85a5ac46c30d10ee5b4a7f06d6b32321e5
DeShon Dixon <deshondixon@gmail.com>
2023-05-02 20:41:58 -0700

    acp

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -3,7 +3,7 @@ import { projects } from '../data';
 
 const Projects = () => {
   return (
-    <div className='px-6 py-2'>
+    <div className='px-5 py-2 overflow-y-scroll' style={{ height: '65vh' }}>
       <nav>Navbar</nav>
       <div className='relative grid grid-cols-12 gap-4 my-3'>
         {projects.map((project) => (
