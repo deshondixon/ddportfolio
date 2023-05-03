3b09264007b85d711d1168cb12e851e28b5ffa3e
DeShon Dixon <deshondixon@gmail.com>
2023-05-02 21:34:26 -0700

    acp

--- components/ProjectsNavbar.tsx
+++ components/ProjectsNavbar.tsx
@@ -1,11 +1,35 @@
-import React from 'react';
+import React, { FunctionComponent } from 'react';
+import { Category } from '../src/type';
+import { Text, Badge } from '@nextui-org/react';
 
-export const NavItem = () => {
-  return <div>ProjectsNavbar</div>;
+export const NavItem: FunctionComponent<{
+  value: Category | 'all';
+  handlerFilterCategory: Function;
+}> = ({ value, handlerFilterCategory }) => {
+  return (
+    <li
+      className='text-xl font-semibold capitalize cursor-pointer text-violet-300 hover:text-violet-400'
+      onClick={() => handlerFilterCategory(value)}
+    >
+      {' '}
+      {value}
+    </li>
+  );
 };
 
-const ProjectsNavbar = () => {
-  return <div>ProjectsNavbar</div>;
+const ProjectsNavbar = (props) => {
+  return (
+    <div className='flex px-3 py-2 space-x-3 overflow-x-auto text-2xl list-none'>
+      <NavItem value='all' {...props} />
+      <NavItem value='react' {...props} />
+      <NavItem value='python' {...props} />
+      <NavItem value='django' {...props} />
+      <NavItem value='golang' {...props} />
+      <NavItem value='mongodb' {...props} />
+      <NavItem value='express' {...props} />
+      <NavItem value='node' {...props} />
+    </div>
+  );
 };
 
 export default ProjectsNavbar;
--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,10 +1,30 @@
 import ProjectCard from '../../components/ProjectCard';
-import { projects } from '../data';
+import ProjectsNavbar from '../../components/ProjectsNavbar';
+import { projects as projectsData } from '../data';
+import { useState } from 'react';
+import { Category } from '../type';
 
 const Projects = () => {
+  const [projects, setProjects] = useState(projectsData);
+  const [active, setActive] = useState('all');
+
+  const handlerFilterCategory = (category: Category | 'all') => {
+    if (category === 'all') {
+      setProjects(projectsData);
+      setActive(category);
+      return;
+    }
+
+    const newArray = projectsData.filter((projects) =>
+      projects.category.includes(category)
+    );
+    setProjects(newArray);
+    setActive(category);
+  };
+
   return (
     <div className='px-5 py-2 overflow-y-scroll' style={{ height: '65vh' }}>
-      <nav>Navbar</nav>
+      <ProjectsNavbar handlerFilterCategory={handlerFilterCategory} />
       <div className='relative grid grid-cols-12 gap-4 my-3'>
         {projects.map((project) => (
           <div
