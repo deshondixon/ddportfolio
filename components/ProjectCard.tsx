0630fad76fdf2963ac22dce99397b6ab3150ccaa
DeShon Dixon <deshondixon@gmail.com>
2023-05-03 18:37:17 -0700

    acp

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -3,6 +3,8 @@ import { IProject } from '../src/type';
 import { AiFillGithub, AiFillProject } from 'react-icons/ai';
 import { Modal, Image, Button, Text, Spacer, Badge } from '@nextui-org/react';
 import React from 'react';
+import { motion } from 'framer-motion';
+import { fadeInUp, stagger } from '../src/animations';
 
 const ProjectCard: FunctionComponent<{
   project: IProject;
@@ -45,60 +47,66 @@ const ProjectCard: FunctionComponent<{
           {name}
         </Text>
       </Button>
+      <motion.div variants={stagger} initial='initial' animate='animate'>
+        <Modal
+          scroll
+          width='800px'
+          aria-labelledby='modal-title'
+          aria-describedby='modal-description'
+          closeButton
+          blur
+          className='flex items-center'
+          open={visible}
+          onClose={closeHandler}
+        >
+          <Modal.Header id='modal-title' className='flex flex-col w-96 h-96'>
+            <Text
+              size={32}
+              css={{
+                textGradient: '45deg, $yellow600 -20%, $red600 100%',
+              }}
+              weight='bold'
+              className='flex items-center justify-center text-3xl tracking-wider '
+            >
+              {name}
+            </Text>
+            <motion.div variants={fadeInUp}>
+              <Image src={image_path} alt={name} width={300} height={300} />
+            </motion.div>
 
-      <Modal
-        scroll
-        width='800px'
-        aria-labelledby='modal-title'
-        aria-describedby='modal-description'
-        closeButton
-        blur
-        className='flex items-center'
-        open={visible}
-        onClose={closeHandler}
-      >
-        <Modal.Header id='modal-title' className='flex flex-col w-96 h-96'>
-          <Text
-            size={32}
-            css={{
-              textGradient: '45deg, $yellow600 -20%, $red600 100%',
-            }}
-            weight='bold'
-            className='flex items-center justify-center text-3xl font-medium tracking-wider'
-          >
-            {name}
-          </Text>
-          <Image src={image_path} alt={name} width={800} height={800} />
-          <div className='flex flex-row'>
-            <a href={github_url}>
-              <AiFillGithub className='w-8 h-8' /> <span>Github</span>
-            </a>
-            <Spacer />
-            <a href={deployed_url}>
-              <AiFillProject className='w-8 h-8' /> <span>Project</span>
-            </a>
-          </div>
-        </Modal.Header>
+            <motion.div className='flex flex-row' variants={fadeInUp}>
+              <a href={github_url}>
+                <AiFillGithub className='w-8 h-8' /> <span>Github</span>
+              </a>
+              <Spacer />
+              <a href={deployed_url}>
+                <AiFillProject className='w-8 h-8' /> <span>Project</span>
+              </a>
+            </motion.div>
+          </Modal.Header>
 
-        <Modal.Body>
-          <Text id='modal-description'>{description}</Text>
-        </Modal.Body>
-        <Modal.Footer>
-          <div className='flex flex-wrap text-sm '>
-            {key_techs.map((tech) => (
-              <Badge
-                color='secondary'
-                disableOutline
-                className='px-2 py-1 '
-                variant='flat'
-                key={tech}
-              >
-                {tech}
-              </Badge>
-            ))}
-          </div>
-        </Modal.Footer>
-      </Modal>
+          <Modal.Body>
+            <motion.div variants={fadeInUp}>
+              <Text id='modal-description'>{description}</Text>
+            </motion.div>
+          </Modal.Body>
+          <Modal.Footer>
+            <div className='flex flex-wrap text-sm '>
+              {key_techs.map((tech) => (
+                <Badge
+                  color='secondary'
+                  disableOutline
+                  className='px-2 py-1 '
+                  variant='flat'
+                  key={tech}
+                >
+                  {tech}
+                </Badge>
+              ))}
+            </div>
+          </Modal.Footer>
+        </Modal>
+      </motion.div>
     </div>
   );
 };
