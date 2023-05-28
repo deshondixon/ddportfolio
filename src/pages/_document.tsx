2cf9fbed1902b7211c0156e79841bd2fd8b4246b
DeShon Dixon <deshondixon@gmail.com>
2023-05-27 23:39:36 -0700

    acp

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,9 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang='en'>
-        <Head></Head>
+        <Head>
+          <meta name='description' content='DeShon Dixon Portfolio Site' />
+        </Head>
         <body>
           <Main />
           <NextScript />
