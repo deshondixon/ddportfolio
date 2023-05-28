5a2afbcd50c98165e72eeaff4dfa98f98633091b
DeShon Dixon <deshondixon@gmail.com>
2023-05-27 22:09:22 -0700

    acp

--- src/pages/resume.tsx
+++ src/pages/resume.tsx
@@ -78,7 +78,7 @@ const resume = () => {
                 color: 'gray100',
               }}
             >
-              Software Engineer
+              Associate Software Engineer
             </Text>
             <Text
               size={20}
