e2c664b16838fe2f9a73fb16eb265e146b5371b0
DeShon Dixon <deshondixon@gmail.com>
2023-05-27 22:37:57 -0700

    acp

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,7 +11,7 @@ import 'tailwindcss/tailwind.css';
 import { AnimatePresence } from 'framer-motion';
 
 const darkTheme = createTheme({
-  type: 'dark',
+  type: 'light',
   theme: {},
 });
 
@@ -33,7 +33,7 @@ export default function MyApp({ Component, pageProps, router }: AppProps) {
         }}
       >
         <NextUIProvider>
-          <div className='grid grid-cols-12 gap-6 px-5 text-white pt-14 lg:px-48'>
+          <div className='grid grid-cols-12 gap-6 px-5 pt-14 lg:px-48'>
             <div className='col-span-12 p-4 text-center lg:col-span-3 rounded-2xl glass'>
               {loading ? <Skeleton height={500} /> : <Sidebar />}
             </div>
