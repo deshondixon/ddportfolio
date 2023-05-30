4524e2a862cdff0a12b23e0b130ad6f6a6aacbfc
DeShon Dixon <deshondixon@gmail.com>
2023-05-30 14:05:23 -0700

    acp

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -27,7 +27,21 @@ const Projects = () => {
 
   return (
     <>
-     
+      <Head>
+        <title>Projects</title>
+      </Head>
+      <motion.div
+        className='px-5 py-2 overflow-y-scroll'
+        style={{ height: '65vh' }}
+        variants={routeAnimation}
+        animate='animate'
+        initial='initial'
+        exit='exit'
+      >
+        <ProjectsNavbar
+          handlerFilterCategory={handlerFilterCategory}
+          active={active}
+        />
         <motion.div
           className='relative grid grid-cols-12 gap-4 my-3'
           variants={stagger}
